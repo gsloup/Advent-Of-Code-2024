4a2554190a05d200b048046beb791c6d856bb035
Garrett Sloup <garrettsloup@gmail.com>
2024-12-05 16:24:02 -0600

    solves problem

--- day1/problem1/solution.js
+++ day1/problem1/solution.js
@@ -1,28 +1,46 @@
-const fs = require('fs');
-const readline = require('readline')
+import fs from 'fs';
+import readline from 'readline';
 
-const rl = readline.createInterface({
-    input: fs.createReadStream('./input.txt'),
-    output: process.stdout,
-    terminal: false
-});
-
-let firstList = [];
-let secondList = [];
-
-rl.on('line', (line) => {
-    const fields = line.split(/\s+/);
-    const firstNum = Number(fields[0]);
-    const secondNum = Number(fields[1]);
-
-    // console.log(firstNum)
+/*
+    Reads the input.txt file line by line, sorting the left and right column
+        of numbers into their own array
+*/
+async function processInputFile() {
+    let firstList = [];
+    let secondList = [];
     
-    firstList.push(firstNum);
-    secondList.push(secondNum);
-});
+    const rl = readline.createInterface({
+        input: fs.createReadStream('./input.txt'),
+        output: process.stdout,
+        terminal: false
+    });
+    
+    rl.on('line', (line) => {
+        const fields = line.split(/\s+/);
+        const [ firstNum, secondNum ] = fields.map(Number);
+        
+        firstList.push(firstNum);
+        secondList.push(secondNum);
+    });
 
-rl.on('close', () => {
-    console.log("Done");
-    console.log(firstList)
-});
+    await new Promise((resolve) => rl.on('close', resolve));
 
+    return { firstList, secondList };
+}
+// -----------------------------------------------------------
+
+const { firstList, secondList } = await processInputFile();
+
+// Sort each num array into ascending order
+firstList.sort(function(a, b){return a-b});
+secondList.sort(function(a, b){return a-b});
+
+let sumOfDifferences = 0;
+
+// Compare each corresponding value from one list to the other, adding the differences between them
+firstList.map((firstValue, index) => {
+    const secondValue = secondList[index];
+    sumOfDifferences += Math.abs(firstValue - secondValue);
+})
+
+console.log("The total sum of differences between the two compared lists: ", sumOfDifferences)
