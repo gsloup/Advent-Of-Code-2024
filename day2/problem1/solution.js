29d1e028aa709a1e1df148b4b223305e0fc1517f
Garrett Sloup <garrettsloup@gmail.com>
2024-12-10 23:41:02 -0600

    working through it

--- day2/problem1/solution.js
+++ day2/problem1/solution.js
@@ -1,27 +1,57 @@
-import fs from 'fs';
-import readline from 'readline';
+// import fs from 'fs';
+// import readline from 'readline';
 
-export async function processInputFile() {
-    let reports = [];
+// export async function processInputFile() {
+//     let reports = [];
 
-    const rl = readline.createInterface({
-        input: fs.createReadStream('./../input.txt'),
-        output: process.stdout,
-        terminal: false
-    });
+//     const rl = readline.createInterface({
+//         input: fs.createReadStream('./../input.txt'),
+//         output: process.stdout,
+//         terminal: false
+//     });
     
-    rl.on('line', (line) => {
-        const fields = line.split(' ');
-        reports.push(fields.map(Number));
-    });
+//     rl.on('line', (line) => {
+//         const fields = line.split(' ');
+//         reports.push(fields.map(Number));
+//     });
 
-    await new Promise((resolve) => rl.on('close', resolve));
+//     await new Promise((resolve) => rl.on('close', resolve));
 
-    return reports;
-}
-const reports = await processInputFile();
-console.log( reports );
+//     return reports;
+// }
+// const reports = await processInputFile();
+// console.log( reports );
+const reports = [[
+    42, 44, 47, 49,
+    51, 52, 54, 52
+  ]]
+
+let safeReport = 0;
 
 reports.forEach((report) => {
-    
+    let status = 'safe'; // can be 'increasing', 'decreasing', 'unsafe', 'safe'
+
+    for (let i = 0; i < report.length; i++) {
+        const currentLevel = report[i];
+        console.log("current level =", report[i]);
+
+        if (i > 0) {
+            const prevLevel = report[i-1];
+            if (prevLevel === currentLevel ||
+                (prevLevel > currentLevel && status === "")
+
+            ) {
+                status = 'unsafe';
+                break;
+            }
+        }
+        if (status = 'unsafe') {
+            break;
+        }
+        
+    }
+    // add the 'safe' counter here?
+    if (status === 'safe') {
+        safeReport++;
+    }
 });
