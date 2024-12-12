92a4a7bdf4aca0904d7cd96e102902e91ff028bd
Garrett Sloup <garrettsloup@gmail.com>
2024-12-11 23:33:37 -0600

    more work

--- day2/problem1/solution.js
+++ day2/problem1/solution.js
@@ -22,36 +22,46 @@
 // const reports = await processInputFile();
 // console.log( reports );
 const reports = [[
-    42, 44, 47, 49,
-    51, 52, 54, 52
+    42, 44, 47, 49, 51, 52, 54, 52
   ]]
 
-let safeReport = 0;
+let safeReport = 0; // total num of safe reports
 
 reports.forEach((report) => {
-    let status = 'safe'; // can be 'increasing', 'decreasing', 'unsafe', 'safe'
+    let status; // can be 'increasing', 'decreasing'
+    let safe = true; // if still true at the end, add the point
 
     for (let i = 0; i < report.length; i++) {
         const currentLevel = report[i];
-        console.log("current level =", report[i]);
+        console.log("current level =", currentLevel);
 
         if (i > 0) {
             const prevLevel = report[i-1];
-            if (prevLevel === currentLevel ||
-                (prevLevel > currentLevel && status === "")
-
-            ) {
-                status = 'unsafe';
-                break;
+            console.log("prev level =", prevLevel);
+            // 2 consecutive nums can be equal or different by more than 3
+            if ((prevLevel === currentLevel) || (Math.abs(prevLevel - currentLevel) > 3)) {
+                console.log("NUM ERROR");
+                safe = false;
+            // change in status from increasing -> decreasing
+            } else if ((prevLevel > currentLevel) && status === 'decreasing') {
+                console.log("Status ERROR 1");
+                safe = false;
+            // change in status from decreasing -> increasing
+            } else if ((prevLevel < currentLevel) && status === 'increasing') {
+                console.log("Status ERROR 2");
+                safe = false;
             }
         }
-        if (status = 'unsafe') {
+        if (!safe) {
+            console.log('NOT SAFE')
             break;
         }
         
     }
     // add the 'safe' counter here?
-    if (status === 'safe') {
+    if (safe) {
         safeReport++;
     }
 });
+
+console.log("Num of safe reports: ", safeReport);
